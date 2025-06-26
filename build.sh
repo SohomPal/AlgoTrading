a18ca4d4b472ecd5a960e507556de2bebbeb17d0
SohomPal <sohompal21@gmail.com>
2025-06-25 23:43:40 -0400

    Hopefully works

--- build.sh
+++ build.sh
@@ -5,6 +5,14 @@ set -e  # Exit on first error
 echo "Cleaning old build..."
 rm -rf build
 
+echo "Cleaning gRPC artifacts..."
+rm -r grpc/*.cc
+rm -r grpc/*.h
+cd grpc
+protoc -I=. --cpp_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` orderbook.proto
+cd ..
+
+
 echo "Creating build directory..."
 mkdir build
 cd build
